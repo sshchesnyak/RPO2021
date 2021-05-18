4fbedf798c482b1bcf1863c52feeb403e6365d8e
Sergey <sshchesnyak@gmail.com>
2021-05-19 01:28:14 +0300

    Http commit

---
+++ libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/debug.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/debug.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/net_sockets.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/net_sockets.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_cache.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_cache.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_ciphersuites.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_ciphersuites.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_cli.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_cli.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_cookie.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_cookie.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_msg.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_msg.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_srv.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_srv.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_ticket.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_ticket.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_tls.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_tls.c.o"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library/ssl_tls13_keys.c" "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedtls.dir/ssl_tls13_keys.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "mbedtls_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/include"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/mbedtls/library"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedx509.dir/DependInfo.cmake"
+  "/home/sshchesnyak/Documents/Android-projects/libs/mbedtls/build/x86/library/CMakeFiles/mbedcrypto.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
